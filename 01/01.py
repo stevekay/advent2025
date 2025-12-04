33e64c19873524f4b2b0399dfee7c89650cc9235
steve kay <stevekay@gmail.com>
2025-12-04 22:58:14 +0000

    tidyup

--- 01/01.py
+++ 01/01.py
@@ -9,23 +9,22 @@ parttwo = 0
 for b in open(0).readlines():
     direction = b[:1]
     turns = int(b.strip()[1:])
-    z = 0
+
     if direction == 'L':
         for c in range(turns):
             pos -= 1
             if pos == 0 and c+1 < turns:
-                z += 1
+                parttwo += 1
             if pos < 0:
                 pos += 100
     else:
         for c in range(turns):
             pos += 1
             if pos == 100 and c+1 < turns:
-                z += 1
+                parttwo += 1
             if pos > 99:
                 pos -= 100
 
     partone += (pos==0)
-    parttwo += z
 
 print(partone,partone+parttwo)
