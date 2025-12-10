f4b6afe2d3f963b3cc6356db321b748d08b76c00
steve kay <stevekay@gmail.com>
2025-12-10 13:12:31 +0000

    tidyup

--- 03/03.py
+++ 03/03.py
@@ -6,25 +6,12 @@ partone = 0
 parttwo = 0
 
 for bank in open(0).readlines():
-    print(bank.strip())
-    highest = -1
-    highestpos = -1
-    for pos in range(0,len(bank.strip())-1):
-        battery = int(bank[pos])
-        print(" pos=",pos,"value=",battery)
-        if battery > highest:
-            highest = battery
-            highestpos = pos
-    print("highest battery was",highest,"at pos",highestpos)
-    zhighest = -1
-    for pos in range(highestpos+1,len(bank.strip())):
-        battery = int(bank[pos])
-        if battery > zhighest:
-            zhighest = battery
-    print("so best combo was",highest,zhighest)
-    v = highest * 10 + zhighest
-    print(v)
-    partone += v
+    bank = bank.strip()
+
+    highest_voltage = max(bank[:len(bank)-1])
+    highest_position = bank.index(highest_voltage)
+    next_highest_voltage = max(bank[highest_position+1:])
+
+    partone += int(highest_voltage + next_highest_voltage)
 
 print(partone)
-
