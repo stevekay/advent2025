d05f14fd7c35b8898f5600e2d1c6848374619aca
steve kay <stevekay@gmail.com>
2025-12-10 22:46:50 +0000

    day5part1 tidied

--- 05/05.py
+++ 05/05.py
@@ -7,24 +7,17 @@ partone = 0
 ranges = []
 
 for x in open(0).readlines():
-    x = x.strip()
-#    print(x)
+    z = re.match(r'(\d+)-(\d+)', x)
+    if z:
+        ranges.append( [int(z.group(1)), int(z.group(2))] )
 
-    z = re.match(r'(\d+)-(\d+)',x)
-    if z != None:
-        ranges.append( [int(z.group(1)),int(z.group(2))])
-
-    z = re.match(r'(\d+)$',x)
-    if z != None:
+    z = re.match(r'(\d+)$', x)
+    if z:
         i = int(z.group(1))
-#        print("ingredient",i)
         fresh = 0
         for [start,end] in ranges:
             if i >= start and i <= end:
                 fresh = 1
-#                print(" in range")
         partone += fresh
-    
 
-#print(ranges)
 print(partone)
